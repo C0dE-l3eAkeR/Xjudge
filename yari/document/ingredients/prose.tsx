598a55bcacc26bda3e8e5f0a70071e7d317f7a30
C0dE-l3eAkeR <shoyib.hassan@northsouth.edu>
2023-09-05 01:33:21 +0600

    first commit

---
+++ yari/document/ingredients/prose.tsx
@@ -0,0 +1,24 @@
+import { useMemo } from "react";
+import { DisplayH2, DisplayH3 } from "./utils";
+
+export function Prose({ section }: { section: any }) {
+  const { id } = section;
+  const html = useMemo(() => ({ __html: section.content }), [section.content]);
+
+  if (!id) {
+    return <div className="section-content" dangerouslySetInnerHTML={html} />;
+  }
+
+  const DisplayHx = section.isH3 ? DisplayH3 : DisplayH2;
+
+  return (
+    <section aria-labelledby={id}>
+      <DisplayHx
+        id={id}
+        title={section.title}
+        titleAsText={section.titleAsText}
+      />
+      <div className="section-content" dangerouslySetInnerHTML={html} />
+    </section>
+  );
+}
---
+++ yari/plus/offer-overview/offer-hero/index.tsx
@@ -0,0 +1,36 @@
+import { DEV_MODE } from "../../../env";
+import Mandala from "../../../ui/molecules/mandala";
+import "./index.scss";
+
+function OfferHero() {
+  const animate = !DEV_MODE;
+  return (
+    <div className="dark offer-hero">
+      <header className="container offer-hero-header">
+        <div className="offer-hero-wrapper">
+          <h1>
+            More MDN. <u>Your</u> MDN.
+          </h1>
+          <>
+            <h2>
+              Support MDN <u>and</u> make it your own.
+            </h2>
+            <div className="button-wrapper">
+              <a href="#subscribe" className="button-primary">
+                Get started
+              </a>
+              <a href="#features" className="button-secondary">
+                What's included
+              </a>
+            </div>
+          </>
+        </div>
+      </header>
+      <div className="mandala-wrapper">
+        <Mandala animate={animate} animateColors={animate} />
+      </div>
+    </div>
+  );
+}
+
+export default OfferHero;
